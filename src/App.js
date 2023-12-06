9f97f43b0e8ffad5714e08d50592c8cab42b4dd3
DineshRamaraj <dineshkumar26032001@gmail.com>
2023-12-06 06:03:06 +0000

    update file

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import UserProfile from './components/UserProfile/index'
+// import UserProfile from './components/UserProfile/index'
+import UserProfile from './components/UserProfile'
 
 import './App.css'
 
@@ -9,21 +10,25 @@ import './App.css'
 // }
 const userDetailsList = [
   {
+    uniqueNo: 1,
     imageUrl: 'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
     name: 'Dineshkumar',
     role: 'Software Developer',
   },
   {
+    uniqueNo: 2,
     imageUrl: 'https://assets.ccbp.in/frontend/react-js/floyd-miles-img.png',
     name: 'Surya Kumar',
     role: 'Software Developer',
   },
   {
+    uniqueNo: 3,
     imageUrl: 'https://assets.ccbp.in/frontend/react-js/jacob-jones-img.png',
     name: 'Arun Kumar',
     role: 'Software Developer',
   },
   {
+    uniqueNo: 4,
     imageUrl: 'https://assets.ccbp.in/frontend/react-js/devon-lane-img.png',
     name: 'Sarath Kumar',
     role: 'Software Developer',
@@ -35,7 +40,7 @@ const App = () => (
     <h1 className="title">Users List</h1>
     <ul>
       {userDetailsList.map(eachItem => (
-        <UserProfile UserDetails={eachItem} />
+        <UserProfile userDetails={eachItem} key={eachItem.uniqueNo} />
       ))}
     </ul>
     {/* <ul>
